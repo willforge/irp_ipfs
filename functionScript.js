0a97e12212a1b9056b61229d30b4c231896a3d67
Morgan Z. Critcher <mcritcher@hellodoc.tk>
2020-06-24 18:59:34 +0200

    toggle pins is now working

--- functionScript.js
+++ functionScript.js
@@ -22,6 +22,7 @@ async function getStatofMfsPath(mfs_path) {
 
 async function provide_directory_content() {
   let mfs_path = getInputValue('mfs_pathinputid');
+  mfs_path = mfs_path.replace(new RegExp('[^/]+/\.\./'),'')
   
   // build directory content ...
   return build_directory_content(mfs_path);
@@ -29,17 +30,46 @@ async function provide_directory_content() {
 }
 
 function build_directory_content(mfs_path) {
-  let  url = api_url + 'files/ls?arg='+mfs_path+'&long=true&U=true'
-  return fetchRespCatch(url)
-  .then(obj => {
+  let parent_path = mfs_path + '../';
+  if (mfs_path == '/') {
+  parent_path = '/';
+  }
+  let promise_parent_hash = getHashofMfsPath(parent_path);
+  //console.log('buid_dc.parent_hash: '+parent_hash);
+  let url = api_url + 'files/ls?arg='+mfs_path+'&long=true&U=true'
+  let promise_dir_content = fetchRespCatch(url)
+
+  return Promise.all([promise_parent_hash,promise_dir_content]) 
+  .then(_ => {
+     [parent_hash, obj] = _
+     console.log('build_directory_content.promise.results: ',_);
      let table_of_content = obj.Entries;
+     if (table_of_content == null) {
+       table_of_content = [];
+     }
+     let parent_item = { Name:'..', Type:1, Size:0, Hash:parent_hash, Path:mfs_path }
+     console.log('build_directory_content.parent_item: ',parent_item);
+     table_of_content.unshift(parent_item)
      console.log('build_directory_content: ',table_of_content);
-     return { "dirname":mfs_path,"TOC":table_of_content };
+     return { "dirname":mfs_path, "parent":parent_hash, "TOC":table_of_content };
 
 
    })
    .catch( obj => { logError('build_directory_content.catch',obj) })
 }
+function getHashofMfsPath(mfs_path) {
+  let  url = api_url + 'files/stat?arg='+mfs_path+'&hash=true'
+  return fetchGetPostJson(url) // get
+     .then( json => {
+         return json.Hash
+     })
+}
+
+function provide_file_content() {
+  let mfs_path = getInputValue('mfs_pathinputid');
+  return getContentofMfsPath(mfs_path);
+}
+
 
 function provideItem(ofwhat) {
   if (typeof(stored[ofwhat]) != 'undefined') {  
@@ -67,8 +97,8 @@ async function providePinStatus(ofwhat) {
 }
 
 async function provideHashofMfsPath(ofwhat) {
-  if (typeof(stored[ofwhat].hash) != 'undefined') {  
-    return stored[ofwhat].hash
+  if (typeof(stored[ofwhat].Hash) != 'undefined') {  
+    return stored[ofwhat].Hash
   } else {
      let mfs_path = stored[ofwhat].mfs_path
      console.log('provideHashof....stored['+ofwhat+']: ',stored[ofwhat]);
@@ -76,7 +106,7 @@ async function provideHashofMfsPath(ofwhat) {
      let  url = api_url + 'files/stat?arg='+mfs_path+'&hash=true'
      let hash = await fetchGetPostJson(url) // buid/get
      .then( json => {
-         stored[ofwhat].hash = json.Hash
+         stored[ofwhat].Hash = json.Hash
          return json.Hash
      })
      .catch( console.error )
@@ -102,18 +132,28 @@ function getPinStatus(hash) { // getdata
 }
 
 function togglePinStatus(status, hash) {
-   if (status == 'unpinned' || status == 'indirect-through') {
+   let matches = status.match(/through (\w+)/)
+   let qmthrough
+   if (matches) {
+     qmthrough = matches[1]
+     console.log('through-qm: '+qmthrough)
+     
+     status = 'indirect'
+   }
+   if (status == 'unpinned' || status == 'indirect') {
       return ipfsPinAdd(hash)
       .then( _ => { return getPinStatus(hash)})
 
-   } else if (status == 'direct' || status == 'recursive' || status == 'indirect-through')  {
+   } else if (status == 'direct' || status == 'recursive' || status == 'indirect')  {
       return ipfsPinRm(hash)
       .then( _ => { return getPinStatus(hash)})
    } else {
       console.log('togglePinStatus.status:',status);
-      return '???'
+      return Promise.resolve('???');
    }
 }
+
+
 function ipfsPinAdd(hash) {
    let url = api_url + 'pin/add?arg=/ipfs/'+hash+'&progress=true'
    return fetchGetPostText(url)
